54b3f69f7af1967a444e2772ba2dfc3976773c56
lbssn <150823579+lbssn@users.noreply.github.com>
2024-03-27 15:20:33 +0100

    Delete BlobInspector directory

--- BlobInspector/BlobInspectorWindows.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-python appli.py
-pause
--- BlobInspector/install_packages.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-packages = ['pyside6', 'scikit-image', 'numpy', 'matplotlib', 'scipy']
-
-for package in packages:
-    subprocess.check_call(['pip', 'install', package])
